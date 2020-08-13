8398a0d09055bf883ecadce8a954ce4630f7b86e
yachialice <2441123794@qq.com>
2020-08-13 10:44:23 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import torch
+import numpy as np
+from PIL import Image
+import matplotlib
+print(torch.__version__)
